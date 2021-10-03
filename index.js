e50875c0d0a2d44cf7076ae81d8fea0ad52ccb2d
JohnBensen1000 <jbensen@purdue.edu>
2021-10-03 14:15:48 -0400

    added code for testing purposes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function main() {
-    console.log("Hello world")
+    // The following is intentially badly written code. The purpose of this
+    // is to see if the correctness metric calculations detects this as bad code. 
+    const baba = "baba"
+    const str1 = old_str1.replaceAll(baba, "    ");
 }
